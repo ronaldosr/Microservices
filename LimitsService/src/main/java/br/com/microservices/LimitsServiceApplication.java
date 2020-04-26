2f4a5bc83a51cce3dde984c21ef3bca885b8ffbb
ronaldosr <ronaldosr@users.noreply.github.com>
2020-04-26 17:27:55 -0300

    Incluindo o limit service no eureka

--- LimitsService/src/main/java/br/com/microservices/LimitsServiceApplication.java
+++ LimitsService/src/main/java/br/com/microservices/LimitsServiceApplication.java
@@ -2,8 +2,12 @@ package br.com.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RibbonClient(name="limits-service")
 public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
