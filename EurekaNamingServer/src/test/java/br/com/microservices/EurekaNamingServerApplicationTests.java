01e168d8f0dafe65d4bd0adfc81803853a75a7ed
ronaldosr <ronaldosr@users.noreply.github.com>
2020-04-26 14:36:11 -0300

    inclusao do Eureka

---
+++ EurekaNamingServer/src/test/java/br/com/microservices/EurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
