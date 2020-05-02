9c03788db64cb05c722f3621d8112344d57d261c
ronaldosr <ronaldosr@users.noreply.github.com>
2020-05-02 13:28:34 -0300

    Adicionando tolerancia a falhas

--- LimitsService/src/main/java/br/com/microservices/LimitsServiceApplication.java
+++ LimitsService/src/main/java/br/com/microservices/LimitsServiceApplication.java
@@ -3,11 +3,13 @@ package br.com.microservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RibbonClient(name="limits-service")
+@EnableHystrix
 public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
--- LimitsService/src/main/java/br/com/microservices/controller/LimitsConfigurationController.java
+++ LimitsService/src/main/java/br/com/microservices/controller/LimitsConfigurationController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import br.com.microservices.bean.LimitConfiguration;
 import br.com.microservices.config.Configuration;
 
@@ -17,5 +19,15 @@ public class LimitsConfigurationController {
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
 		return new LimitConfiguration(
 				config.getMaximum(), config.getMinimum());
+	}
+	
+	@GetMapping("/limits-fault")
+	@HystrixCommand(fallbackMethod ="fallbackRetrieveLimitsFaultFromConfigurations" )
+	public LimitConfiguration retrieveLimitsFaultFromConfigurations() {
+		throw new RuntimeException("Not Available");
+	}
+	
+	public LimitConfiguration fallbackRetrieveLimitsFaultFromConfigurations() {
+		return new LimitConfiguration(999, 9);
 	}
 }
